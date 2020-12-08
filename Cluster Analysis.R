2dd8eeefd6a0b7ba37dc0b8e69da40a3a9e12d8c
Junjie-Dylan-Yang <dylanyang95@gmail.com>
2020-12-08 16:21:47 -0500

    Organize

--- Cluster Analysis.R
+++ Cluster Analysis.R
@@ -6,6 +6,11 @@ building_metadata <- read_csv(here("data","filled_bldg_meta.csv"))
 building_meter <- read_csv(here("data", "train.csv"))
 weather <- read_csv(here("data", "weather_train.csv"))
 
+building_metadata = read.csv("C:/Users/we704/Desktop/filled_bldg_meta.csv")
+weather = read.csv("C:/Users/we704/Desktop/weather_train.csv")
+building_meter = read.csv("C:/Users/we704/Desktop/train.csv")
+
+
 building <- building_meter %>%
   left_join(building_metadata, by = 'building_id') %>%
   mutate(site_id = as_factor(site_id))
@@ -24,7 +29,7 @@ tmp <- building %>%
               values_from = c(avg_meter_reading, sd_meter_reading)) %>%
   left_join(building_metadata, by = 'building_id') %>% 
   filter(primary_use == 'Education') %>%
-  select(-X1, -site_id, -primary_use, -floor_count, -square_feet, -year_built)
+  select(-X, -site_id, -primary_use, -floor_count, -square_feet, -year_built)
 
 e_mat <- tmp %>% select(-building_id) %>% as.matrix()
 
@@ -49,8 +54,25 @@ building %>%
   summarize(avg_meter_reading = mean(meter_reading)) %>%
   mutate(date = ymd_h(paste0("2020-11-",day, " ", hour))) %>%
   ggplot(aes(x=date, y=avg_meter_reading)) + 
-  geom_path()
+  geom_path()+ggtitle("Cluster 1 Usage Pattern")
 
+building %>% 
+  filter(meter == 0, building_id %in% tmpg2) %>%
+  mutate(day = wday(timestamp), hour = hour(timestamp)) %>%
+  group_by(primary_use, day, hour) %>% 
+  summarize(avg_meter_reading = mean(meter_reading)) %>%
+  mutate(date = ymd_h(paste0("2020-11-",day, " ", hour))) %>%
+  ggplot(aes(x=date, y=avg_meter_reading)) + 
+  geom_path()+ggtitle("Cluster 2 Usage Pattern")
+
+building %>% 
+  filter(meter == 0, building_id %in% tmpg3) %>%
+  mutate(day = wday(timestamp), hour = hour(timestamp)) %>%
+  group_by(primary_use, day, hour) %>% 
+  summarize(avg_meter_reading = mean(meter_reading)) %>%
+  mutate(date = ymd_h(paste0("2020-11-",day, " ", hour))) %>%
+  ggplot(aes(x=date, y=avg_meter_reading)) + 
+  geom_path()+ggtitle("Cluster 3 Usage Pattern")
 
 
 building %>% 
@@ -62,3 +84,4 @@ building %>%
   mutate(date = ymd_h(paste0("2020-11-",day, " ", hour))) %>%
   ggplot(aes(x=date, y=avg_meter_reading)) + 
   geom_path()
+
